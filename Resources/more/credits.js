5e90494f2fd355fe57f837d5e89aa7efc5c56e5b
Tim Shedor <tshedor@gmail.com>
2012-08-31 11:44:20 -0500

    Add FFA icon, adjust credits accordingly, change KUJH tab to FFA

--- Resources/more/credits.js
+++ Resources/more/credits.js
@@ -5,9 +5,8 @@ var win = Ti.UI.currentWindow;
 
 var tv = Ti.UI.createTableView({minRowHeight:80,rowBackgroundColor:'white',backgroundColor:'white',});
 
-var text1 = 'This app was produced by Tim Shedor, Kansan.com Web Editor Fall 2011, Freelance Technical Editor 2012.';
-var tixt2 = 'It is made up of code, images, and other parts that are protected by the Creative Commons license and the Open Source License, produced by the Open Source Initiative. The radio tower icon (appearing above the KJHK tab) was produced by PixelPress Media Design (www.pixelpressicons.com). The bargains and info icons (appearing above the Bargains and More tabs, respectively) was produced by The Working Group (www.theworkinggroup.ca). The weather information is provided by the Google Weather API service and is displayed using foundational code written by Bob Sims. The barcode scanning tool was written with foundational code from the ZBar proejct.';
-var taxt2 = 'It is made up of code, images, and other parts that are protected by the Creative Commons license and the Open Source License, produced by the Open Source Initiative. The radio tower icon (appearing above the KJHK tab) was produced by PixelPress Media Design (www.pixelpressicons.com). The bargains and info icons (appearing above the Bargains and More tabs, respectively) was produced by The Working Group (www.theworkinggroup.ca). The weather information is provided by the Google Weather API service and is displayed using foundational code written by Bob Sims. The barcode scanning tool was written with foundational code from M-Way Solutions.';
+var text1 = 'This app was produced by Tim Shedor, Kansan.com Web Editor Fall 2011, Designer and Technical Editor 2012. Colby Soden, Kansan.com Interactive Developer Fall 2012, wrote foundational code for the Safebus routes.';
+var text2 = 'It is made up of code, images, and other parts that are protected by the Creative Commons license and the Open Source License, produced by the Open Source Initiative. The Free For All icon was created by John Caserta, from The Noun Project. The LarryvilleKU icon was created by our talented in-house digital creative, Andres Rivas-Cruz. The radio tower icon (appearing above the KJHK tab) was produced by PixelPress Media Design (www.pixelpressicons.com). The  info icon was produced by The Working Group (www.theworkinggroup.ca). The weather information is provided by the Google Weather API service and is displayed using foundational code written by Bob Sims.';
 
 var data = [];
 
@@ -28,27 +27,14 @@ var data = [];
 		height:'auto'
 	});
 	textView.add(l1);
-if (Titanium.Platform.name == 'android') {
 	var l2 = Ti.UI.createLabel({
-		text:taxt2,
+		text:text2,
 		top:10,
 		color:'black',
 		height:'auto',
 		bottom:20,
 	});
-	textView.add(l2);
-};
-if (Titanium.Platform.name == 'iPhone OS') {
-	var l2 = Ti.UI.createLabel({
-		text:tixt2,
-		top:10,
-		color:'black',
-		height:'auto'
-	});
-	textView.add(l2);
-};
-
-	
+	textView.add(l2);	
 	row.add(textView);
 
 	data.push(row);
