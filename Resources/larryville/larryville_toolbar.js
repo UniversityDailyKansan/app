92659bba308501ac9c9897350943b98124887581
Tim Shedor <tshedor@gmail.com>
2012-10-19 15:04:54 -0500

    remove anchor point from lv; set the current (upArrow) to retrieve left prop from stored property; merge some click event listeners; organize the scroll event listener (make listen to greater for right - 160 -> 760); make lv scroll view longer;

--- Resources/larryville/larryville_toolbar.js
+++ Resources/larryville/larryville_toolbar.js
@@ -12,33 +12,32 @@ if(Ti.Platform.osname=='android') {
 // HORIZONTAL SCROLLING TABS
 //
 var scrollView = Titanium.UI.createScrollView({
-	anchorPoint:{x:.5,y:.5},
-	contentWidth:400,
+	contentWidth:660,
 	contentHeight:40,
 	top:0,
-	left:40,
+	left:35,
 	height:40,
 	width:'100%',
 	backgroundColor:'#10354c'
 });
-
 win.add(scrollView);
 
 var upArrow = Titanium.UI.createView({
 	backgroundImage:'/images/currentTriangle.png',
-		top:30,
-		left:Ti.App.arrowLV,
-		height: 10,
-		width:20
+	top:30,
+	left:Titanium.App.Properties.getString('currLVTB'),
+	height: 10,
+	width:20
 });
+Ti.API.info(Titanium.App.Properties.getString('currLVTB'));
+scrollView.add(upArrow);
 
 var shadowLeft = Titanium.UI.createView({
 	backgroundImage:'/images/toolbarShadowLeft.png',
 	top:0,
-	left:0,
+	left:35,
 	height:40,
-	width:80,
-	visible:false
+	width:40
 });
 
 win.add(shadowLeft);
@@ -48,43 +47,28 @@ var shadowRight = Titanium.UI.createView({
 	top:0,
 	right:0,
 	height:40,
-	width:80,
-	visible:false
+	width:40
 });
 
 win.add(shadowRight);
 
-var news = Titanium.UI.createLabel({
-			text:'Your News',
-			left:10,
-			height:30,
-			color:'#f9f9f9',
-			font:{
-				fontSize:22,
-				fontFamily:dom,
-			},
-			width:'auto',
-			verticalAlign:'center',
-			textAlign:'center',
-			shadowColor:'#000000',
-			shadowOffset:{x:1, y:-1},
-			top:10
-		});
-	news.addEventListener('click', function(e){
-    	Titanium.App.Analytics.trackPageview('/larryville/news');
-	});
-	news.addEventListener('click',function(e){
-    	var newswindow = Ti.UI.createWindow({
-            url:"feed_view.js",
-            title:"Your News"
-    });
-		Titanium.UI.currentTab.open(newswindow,{animated:true});
-		Ti.App.arrowLV = 90;
-	});
-scrollView.add(news);
+scrollView.addEventListener('scroll', function(e) {
+	if (e.x > 55) {
+		shadowLeft.show();
+	} else {
+		shadowLeft.hide();
+	}
+	if (e.x < 460) {
+		shadowRight.show();
+	} else {
+		shadowRight.hide();
+	}
+});
+
+
 var location = Titanium.UI.createLabel({
 			text:'Your Town',
-			left:145,
+			left:30,
 			height:30,
 			color:'#f9f9f9',
 			font:{
@@ -100,19 +84,44 @@ var location = Titanium.UI.createLabel({
 		});
 	location.addEventListener('click', function(e){
     	Titanium.App.Analytics.trackPageview('/larryville/town');
-	});
-	location.addEventListener('click',function(e){
     	var locationwindow = Ti.UI.createWindow({
             url:"map.js",
             title:"Your Town"
     });
 		Titanium.UI.currentTab.open(locationwindow,{animated:true});
-		Ti.App.arrowLV = 230;
+		Titanium.App.Properties.setString('currLVTB', 75);
 	});
 scrollView.add(location);
+
+var news = Titanium.UI.createLabel({
+			text:'Your News',
+			left:165,
+			height:30,
+			color:'#f9f9f9',
+			font:{
+				fontSize:22,
+				fontFamily:dom,
+			},
+			width:'auto',
+			verticalAlign:'center',
+			textAlign:'center',
+			shadowColor:'#000000',
+			shadowOffset:{x:1, y:-1},
+			top:10
+		});
+	news.addEventListener('click', function(e){
+    	Titanium.App.Analytics.trackPageview('/larryville/news');
+    	var newswindow = Ti.UI.createWindow({
+            url:"feed_view.js",
+            title:"Your News"
+    });
+		Titanium.UI.currentTab.open(newswindow,{animated:true});
+		Titanium.App.Properties.setString('currLVTB', 205);
+	});
+scrollView.add(news);
 var settings = Titanium.UI.createLabel({
 			text:'Settings',
-			left:275,
+			left:305,
 			height:30,
 			color:'#f9f9f9',
 			font:{
@@ -128,20 +137,18 @@ var settings = Titanium.UI.createLabel({
 		});
 	settings.addEventListener('click', function(e){
     	Titanium.App.Analytics.trackPageview('/larryville/settings');
-	});
-	settings.addEventListener('click',function(e){
     	var settingswindow = Ti.UI.createWindow({
             url:"preferences.js",
             title:"Preferences"
     });
 		Titanium.UI.currentTab.open(settingswindow,{animated:true});
-		Ti.App.arrowLV = 340;
+		Titanium.App.Properties.setString('currLVTB', 325);
 	});
 scrollView.add(settings);
 
 var safeRide = Titanium.UI.createLabel({
 			text:'Call SafeRide',
-			left:370,
+			left:420,
 			height:30,
 			color:'#f9f9f9',
 			font:{
@@ -157,36 +164,10 @@ var safeRide = Titanium.UI.createLabel({
 		});
 	safeRide.addEventListener('click', function(e){
     	Titanium.App.Analytics.trackPageview('/larryville/safeRide');
-	});
-	safeRide.addEventListener('click',function(e){
-    	var the_number = '7858647233';
 		Ti.Platform.openURL('tel:+7858647233');
-		Ti.App.arrowLV = 400;
+		Titanium.App.Properties.setString('currLVTB', 395);
 	});
 scrollView.add(safeRide);
 
-scrollView.addEventListener('scroll', function(e)
-{
-    Ti.API.info('x ' + e.x + ' y ' + e.y);
+scrollView.scrollTo((Titanium.App.Properties.getString('currLVTB') - 70), 0);
 
-    if (e.x > 20)
-    {
-        shadowLeft.show();
-    }
-    else
-    {
-        shadowLeft.hide();
-    }
-    if (e.x < 160)
-    {
-        shadowRight.show();
-    }
-    else
-    {
-        shadowRight.hide();
-    }
-
-});
-
-win.add(scrollView);
-win.add(upArrow);
