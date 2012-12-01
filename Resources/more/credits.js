76129a1810184b7f0841d191211d381abf0b4a86
Tim Shedor <tshedor@gmail.com>
2012-12-01 15:56:14 -0600

    Give cred to Sera; bump back SDK to compile properly with iOS

--- Resources/more/credits.js
+++ Resources/more/credits.js
@@ -6,7 +6,7 @@ var win = Ti.UI.currentWindow;
 var tv = Ti.UI.createTableView({minRowHeight:80,rowBackgroundColor:'white',backgroundColor:'white',});
 
 var text1 = 'This app was produced by Tim Shedor, Kansan.com Web Editor Fall 2011, Designer and Technical Editor 2012, and Colby Soden, Kansan.com Interactive Developer Fall 2012.';
-var text2 = 'It is made up of code, images, and other parts that are protected by the Creative Commons license and the Open Source License, produced by the Open Source Initiative. The Free For All icon was created by John Caserta, from The Noun Project. The LarryvilleKU icon was created by our talented in-house digital creative, Andres Rivas-Cruz. The radio tower icon (appearing above the KJHK tab) was produced by PixelPress Media Design (www.pixelpressicons.com). The  info icon was produced by The Working Group (www.theworkinggroup.ca). The weather information is provided by the Google Weather API service and is displayed using foundational code written by Bob Sims.';
+var text2 = 'Interface redesign by Sera Lanzer. It is made up of code, images, and other parts that are protected by the Creative Commons license and the Open Source License, produced by the Open Source Initiative. The Free For All icon was created by John Caserta, from The Noun Project. The LarryvilleKU icon was created by our talented in-house digital creative, Andres Rivas-Cruz. The radio tower icon (appearing above the KJHK tab) was produced by PixelPress Media Design (www.pixelpressicons.com). The  info icon was produced by The Working Group (www.theworkinggroup.ca). The weather information is provided by the Google Weather API service and is displayed using foundational code written by Bob Sims.';
 
 var data = [];
 
