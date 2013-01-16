385fb22e55a709bc8a739f483960a5a40ecd853d
Tim Shedor <tshedor@gmail.com>
2013-01-16 01:30:52 -0600

    migrate a few resources from Resources2/ to app/; build alloy (cli: titanium build -p ios) to export to Resources directory; add controller/view/style (cli [in root project directory]: alloy generate controller {CONTROLLERNAME}) for kjhk, news,  ffa, more

---
+++ Resources/alloy/controllers/pages.js
@@ -0,0 +1,16 @@
+function Controller() {
+    require("alloy/controllers/BaseController").apply(this, Array.prototype.slice.call(arguments));
+    $model = arguments[0] ? arguments[0].$model : null;
+    var $ = this, exports = {}, __defers = {};
+    $.__views.pages = A$(Ti.UI.createView({
+        id: "pages"
+    }), "View", null);
+    $.addTopLevelView($.__views.pages);
+    exports.destroy = function() {};
+    _.extend($, $.__views);
+    _.extend($, exports);
+}
+
+var Alloy = require("alloy"), Backbone = Alloy.Backbone, _ = Alloy._, A$ = Alloy.A, $model;
+
+module.exports = Controller;
